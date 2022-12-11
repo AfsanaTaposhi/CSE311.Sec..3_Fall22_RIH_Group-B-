c2c003b6d1ca5739cad4d8a78cf95ead16762c51
AfsanaTaposhi <taposheeafsana999@gmail.com>
2022-12-12 02:26:23 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Pakh-Pakhali Home</title>
+    <link rel="stylesheet" type="text/css" href="css/style1.css"/>
+  </head>
+  <body>
+    <header>
+      <div class="main">
+        <div class="logo">
+          <a href="index.php"><img src="Images/Logo05.png" alt="#"/></a>
+        </div>
+        <ul>
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="login_signup.php">Login/Signup</a></li>
+        </ul>
+      </div>
+      <div class="title">
+        <h1>Pakh-Pakhali</h1>
+      </div>
+    </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+  </body>
+</html>
